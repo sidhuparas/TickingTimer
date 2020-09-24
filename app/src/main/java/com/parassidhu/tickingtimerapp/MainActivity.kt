2ef03671479ca77fe47d0ff8b24ab24d02042ba4
sidhuparas <sidhuparas1234@gmail.com>
2020-09-24 21:02:02 +0530

    New backgrounds for sample app

--- app/src/main/java/com/parassidhu/tickingtimerapp/MainActivity.kt
+++ app/src/main/java/com/parassidhu/tickingtimerapp/MainActivity.kt
@@ -15,17 +15,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (System.currentTimeMillis() % 2L == 0L)
+            startView1()
+        else
+            startView2()
+    }
+
+    private fun startView1() {
         val config = TickingTimer.defaultConfig().apply {
             timerDuration = 10
             textSize = 50
             textColor = Color.BLACK
             shape = Shape.ROUNDED
+            customBackground = R.drawable.bg_grad_blue
         }
 
         timerView1.start(config)
 
         config.shape = Shape.CIRCLE
         config.timerAnimation = ScaleAnimation(this, null)
+        config.customBackground = R.drawable.bg_grad_orange
+
+        timerView2.start(config)
+    }
+
+    private fun startView2() {
+        val config = TickingTimer.defaultConfig().apply {
+            timerDuration = 10
+            textSize = 50
+            textColor = Color.parseColor("#70FFFFFF")
+            shape = Shape.ROUNDED
+            customBackground = R.drawable.bg_grad_green
+        }
+
+        timerView1.start(config)
+
+        config.shape = Shape.CIRCLE
+        config.timerAnimation = ScaleAnimation(this, null)
+        config.customBackground = R.drawable.bg_grad_pink
 
         timerView2.start(config)
     }
